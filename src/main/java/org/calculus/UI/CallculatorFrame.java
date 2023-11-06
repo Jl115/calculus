bea72e2d82e366e182105d4b2eaa60daeff687e2
Jl115 <joelbern006@gmail.com>
2023-11-06 21:33:23 +0100

    seting the min and maximum size of the frame

--- src/main/java/org/calculus/UI/CallculatorFrame.java
+++ src/main/java/org/calculus/UI/CallculatorFrame.java
@@ -17,7 +17,9 @@ public class CallculatorFrame extends JFrame {
         // adding components to the frame and setting the frame's properties
         this.setTitle("Callculator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(500, 800));
+        this.setPreferredSize(new Dimension(800, 500));
+        this.setMinimumSize(new Dimension(600, 300));
+        this.setMaximumSize(new Dimension(1000, 700));
         this.add(panel, BorderLayout.CENTER);
         this.add(button1, BorderLayout.PAGE_END);
         this.pack();
