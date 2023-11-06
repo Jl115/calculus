fa211f57a81ba2529621b7767cf4ec0c6a8ee16a
Jl115 <joelbern006@gmail.com>
2023-11-06 16:04:34 +0100

    refactoring and setting all up

--- src/main/java/org/calculus/UI/CallculatorFrame.java
+++ src/main/java/org/calculus/UI/CallculatorFrame.java
@@ -4,14 +4,22 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
+import org.calculus.components.BaseButton;
+
 public class CallculatorFrame extends JFrame {
 
     public CallculatorFrame() {
         CallculatorPanel panel = new CallculatorPanel();
+        BaseButton button1 = new BaseButton();
 
+
+
+        // adding components to the frame and setting the frame's properties
+        this.setTitle("Callculator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(500, 800));
         this.add(panel, BorderLayout.CENTER);
+        this.add(button1, BorderLayout.PAGE_END);
         this.pack();
         this.setVisible(true);
     }
---
+++ src/main/java/org/calculus/components/BaseButton.java
@@ -0,0 +1,10 @@
+package org.calculus.components;
+
+import javax.swing.JButton;
+
+public class BaseButton  extends JButton{
+    public BaseButton() {
+        this.setText("Hello World");
+        this.setVisible(true);
+    }
+}
