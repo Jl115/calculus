8b972661472511d0075592eb38fa11aefef05b26
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-11-07 20:08:13 +0100

    Logic for History + 1st Addition Clas for Dima.

--- src/main/java/org/calculus/Main.java
+++ src/main/java/org/calculus/Main.java
@@ -1,7 +1,32 @@
 package org.calculus;
 
+import history.Addition;
+import history.History;
+
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Addition addition = new Addition("5+5+5");
+        Addition addition1 = new Addition("6+8+20+16+55");
+        History history = new History();
+
+        history.addValue(addition.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition1.getRechnung());
+        history.addValue(addition.getRechnung());
+        ArrayList<String> historyia = history.getHistory();
+        for (String value : historyia) {
+            System.out.println(value);
+        }
     }
 }
