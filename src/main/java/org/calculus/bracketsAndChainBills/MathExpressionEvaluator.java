70351a3279b66e45c2f36b997b1f30c0d6cb30fa
Stefan-Mitric <stefan.mitric@ict.csbe.ch>
2023-11-08 19:46:13 +0100

    Implemented logic for calculator

---
+++ src/main/java/org/calculus/bracketsAndChainBills/MathExpressionEvaluator.java
@@ -0,0 +1,78 @@
+package org.calculus.bracketsAndChainBills;
+
+import java.util.Stack;
+
+public class MathExpressionEvaluator {
+
+    public static double evaluate(final String expression) {
+        char[] tokens = expression.toCharArray();
+        Stack<Double> values = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ') {
+                continue;
+            }
+            if (tokens[i] >= '0' && tokens[i] <= '9') {
+                StringBuilder sbuf = new StringBuilder();
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') {
+                    sbuf.append(tokens[i++]);
+                }
+                values.push(Double.parseDouble(sbuf.toString()));
+                i--;
+            } else if (tokens[i] == '(') {
+                ops.push(tokens[i]);
+            } else if (tokens[i] == ')') {
+                while (ops.peek() != '(') {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.pop();
+            } else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek())) {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.push(tokens[i]);
+            }
+        }
+
+        while (!ops.empty()) {
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+
+    public static boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')') {
+            return false;
+        }
+        return !((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'));
+    }
+
+    public static double applyOp(char op, double b, double a) {
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                }
+                else if (a == 0){
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                }
+                return a / b;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MathExpressionEvaluator.evaluate("10 + 2 * 6"));
+        System.out.println(MathExpressionEvaluator.evaluate("100 * 2 + 12"));
+        System.out.println(MathExpressionEvaluator.evaluate("100 * ( 2 + 12 )"));
+        System.out.println(MathExpressionEvaluator.evaluate("100 * ( 2 + 12 ) / 14"));
+    }
+}
