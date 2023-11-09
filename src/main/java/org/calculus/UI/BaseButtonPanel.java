7e023f691f90bebd8ac64ed65bef711ceb938d6d
anukul <sommer.anukul99@gmail.com>
2023-11-09 22:15:35 +0100

    Changed the colour and added gaps

--- src/main/java/org/calculus/UI/BaseButtonPanel.java
+++ src/main/java/org/calculus/UI/BaseButtonPanel.java
@@ -1,8 +1,6 @@
 package org.calculus.UI;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
 
 import javax.swing.JPanel;
 
@@ -14,24 +12,28 @@ public class BaseButtonPanel extends JPanel {
 
     public BaseButtonPanel() {
         setLayout(new GridBagLayout());
+        setBackground(new java.awt.Color(19, 22, 27));
+
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
-        c.insets = new Insets(0, 0,0, 0);
+        c.insets = new Insets(2, 2,2, 2);
         c.weightx = 1.0;
         c.weighty = 1.0;
 
         String[][] buttonLabels = {
-            {"AC", "+/-", "%", "/"},
+            {"AC", "C", "%", "÷"},
             {"7", "8", "9", "x"},
             {"4", "5", "6", "-"},
             {"1", "2", "3", "+"},
-            {"0", ".", "="}
+            {"+/-", "0", ".", "="}
         };
 
         for (int row = 0; row < buttonLabels.length; row++) {
             for (int col = 0; col < buttonLabels[row].length; col++) {
                 String label = buttonLabels[row][col];
                 BaseButton button = new BaseButton(label);
+                button.setBackground(new java.awt.Color(41, 46, 55));
+                button.setForeground(Color.WHITE);
                 c.gridx = col;
                 c.gridy = row;
                 add(button, c);
