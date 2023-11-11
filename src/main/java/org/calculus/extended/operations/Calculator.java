b52a31994b05027f5c531c7318a047b3880e35aa
Jl115 <joelbern006@gmail.com>
2023-11-11 19:26:19 +0100

    clearing up the extendet operations

--- src/main/java/org/calculus/extended/operations/Calculator.java
+++ src/main/java/org/calculus/extended/operations/Calculator.java
@@ -1,9 +1,5 @@
 package org.calculus.extended.operations;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * The Calculator class provides a variety of arithmetic and mathematical functions,
  * including basic operations, modular arithmetic, exponentiation, root extraction,
@@ -12,37 +8,11 @@ import java.util.Map;
 
 public class Calculator {
 
-
-
-    private final Map<String, Double> memory = new HashMap<>(); // Memory storage for calculator values
-
-    /**
-     * Adds two double values.
-     *
-     * @param a The first operand
-     * @param b The second operand
-     * @return The sum of a and b
-     */
-
-
-
-
-
-
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+        calc.factorial(100);
     }
-
-    //  public void divide(double a, double b) {
-    //     if (b == 0) {
-    //         System.out.println("0");
-    //     } else {
-    //         System.out.println(String.valueOf(a/b));
-    //     }
-    // }
     
-
-
     // like this just not in console
     public void modulo(int a, int b) {
         if (b == 0 || a == 0) {
@@ -56,147 +26,21 @@ public class Calculator {
         System.out.println( String.valueOf(Math.pow(base, exponent)));
     }
 
-   
-
-    public static double sqrt(double number) {
+    // like this just not in console
+    public void  squareRoot(double number) {
         if (number < 0) {
-            throw new ArithmeticException("Square root of a negative number is not defined.");
+            System.out.println("Not a Number");
+        }else 
+        System.out.println(Math.sqrt(number));
+    }
+
+    //aproved but not in console
+    public void factorial(double n) {
+        double result = 1.0;
+        for (double i = 2; i <= n; i++) {
+            result *= i;
         }
-        return Math.sqrt(number);
+        System.out.println(result);
     }
-
-    /**
-     * Changes the sign of a number.
-     *
-     * @param number The number to change the sign of.
-     * @return The number with its sign changed.
-     */
-
-    public static double changeSign(double number) {
-        return -number;
-    }
-
-    /**
-     * Calculates the nth root of a number.
-     *
-     * @param n The degree of the root.
-     * @param x The number to find the nth root of.
-     * @return The nth root of x.
-     * @throws ArithmeticException If x is negative and n is even.
-     */
-
-    public static double nthRoot(int n, double x) {
-        if (x < 0 && n % 2 == 0) {
-            throw new ArithmeticException("An even nth root of a negative number is not defined.");
-        }
-        return Math.pow(x, 1.0 / n);
-    }
-
-    /**
-     * Calculates the percentage of a total.
-     *
-     * @param percentage The percentage to calculate.
-     * @param total      The total value.
-     * @return The calculated percentage of the total.
-     */
-
-    public static double percentOf(double percentage, double total) {
-        return (percentage / 100) * total;
-    }
-
-    /**
-     * Squares a number.
-     *
-     * @param number The number to square.
-     * @return The square of number.
-     */
-
-    public static double square(double number) {
-        return Math.pow(number, 2);
-    }
-
-    /**
-     * Calculates the logarithm base 2 of a number.
-     *
-     * @param number The number to calculate the logarithm of.
-     * @return The logarithm base 2 of number.
-     */
-
-    public static double log2(double number) {
-        return Math.log(number) / Math.log(2);
-    }
-
-    /**
-     * Calculates the logarithm base 10 of a number.
-     *
-     * @param number The number to calculate the logarithm of.
-     * @return The logarithm base 10 of number.
-     */
-
-    public static double log10(double number) {
-        return Math.log10(number);
-    }
-
-    /**
-     * Calculates the logarithm of a number with an arbitrary base.
-     *
-     * @param base   The base of the logarithm.
-     * @param number The number to calculate the logarithm of.
-     * @return The logarithm of number with basebase.
-     */
-
-    public static double logBaseN(double base, double number) {
-        return Math.log(number) / Math.log(base);
-    }
-
-    /**
-     * Saves a value with an associated key to the calculator's memory.
-     *
-     * @param key   The key associated with the value.
-     * @param value The value to be saved.
-     */
-
-    public void saveToMemory(String key, double value) {
-        memory.put(key, value);
-    }
-
-    /**
-     * Recalls a value from the calculator's memory.
-     *
-     * @param key The key associated with the value to be recalled.
-     * @return The value associated with the key, or NaN if the key does not exist.
-     */
-
-    public double recallFromMemory(String key) {
-        return memory.getOrDefault(key, Double.NaN);
-    }
-
-    /**
-     * Clears a value from the calculator's memory.
-     *
-     * @param key The key associated with the value to be cleared.
-     */
-
-    public void clearMemory(String key) {
-        memory.remove(key);
-    }
-
-    /**
-     * Calculates the factorial of a non-negative integer.
-     *
-     * @param number The integer to calculate the factorial of.
-     * @return The factorial of number as a BigInteger.
-     * @throws ArithmeticException if the number is negative, as factorial is not defined for negative numbers.
-     */
-
-    public static BigInteger factorial(int number) {
-        if (number < 0) {
-            throw new ArithmeticException("Factorial of negative numbers is not defined.");
-        }
-        BigInteger result = BigInteger.ONE;
-        for (int i = 2; i <= number; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
-        }
-        return result;
-    }
+    
 }
