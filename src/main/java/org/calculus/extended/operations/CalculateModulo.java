1290bda5b3bed10a98687f218d2b2d9e4e5e16aa
Miralem.Ba <miralembanovi@gmail.com>
2023-11-13 17:13:23 +0100

    CalculateModulo class modified

--- src/main/java/org/calculus/extended/operations/CalculateModulo.java
+++ src/main/java/org/calculus/extended/operations/CalculateModulo.java
@@ -1,25 +1,23 @@
 package org.calculus.extended.operations;
 
-    /**
-    * The CalculateModulo class provides a method to calculate the modulo of two numbers.
-     */
-
+/**
+ * The CalculateModulo class provides a method to calculate the modulo (remainder) of two integers.
+ */
 public class CalculateModulo {
 
     /**
      * Calculates the modulo (remainder of the division) of two integers.
      *
-     * @param number1 the dividend
-     * @param number2 the divisor
+     * @param number1 the dividend (the number to be divided)
+     * @param number2 the divisor (the number by which to divide)
      * @return the modulo of number1 and number2
      * @throws IllegalArgumentException if the divisor is 0
      */
-
-    public static int calculateModulo(int number1, int number2) {
-        // It is important to check that number2 is not 0 to prevent division by zero.
-        if (number2 == 0) {
-            throw new IllegalArgumentException("The divisor must not be 0.");
+    public int modulo(int number1, int number2) {
+        if (number2 == 0 || number1 == 0) {
+            throw new IllegalArgumentException("Divisor cannot be 0");
+        } else {
+            return number1 % number2;
         }
-        return number1 % number2;
     }
 }
