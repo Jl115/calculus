50499b10da4fcc7dd6f28fc5e9f1391db0608a79
Jl115 <joelbern006@gmail.com>
2023-11-22 19:23:22 +0100

    praying that this works

--- src/main/java/org/calculus/Main.java
+++ src/main/java/org/calculus/Main.java
@@ -12,26 +12,25 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Addition addition = new Addition("5+5+5");
-        Addition addition1 = new Addition("6+8+20+16+55");
-        History history = new History();
 
-        history.addValue(addition.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition1.getRechnung());
-        history.addValue(addition.getRechnung());
-        ArrayList<String> historyia = history.getHistory();
-        for (String value : historyia) {
-            System.out.println(value);
-        }
+        // History history = new History();
+
+        // history.addValue(addition.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition1.getRechnung());
+        // history.addValue(addition.getRechnung());
+        // ArrayList<String> historyia = history.getHistory();
+        // for (String value : historyia) {
+        //     System.out.println(value);
+        // }
 
         new CallculatorFrame();
     }
