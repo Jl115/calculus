1f09e45a6d50f8d870c37797a698a3e1f4529b1d
anukul <sommer.anukul99@gmail.com>
2023-11-22 19:30:48 +0100

    hover and click effect for the buttons

--- src/main/java/org/calculus/UI/HistoryPanel.java
+++ src/main/java/org/calculus/UI/HistoryPanel.java
@@ -43,12 +43,36 @@ public class HistoryPanel extends JPanel {
         }
 
         // Create the "Close History" button
-        JButton closeButton = new JButton("Close History");
+        JButton closeButton = new JButton("Close History") {
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2.setColor(getBackground());
+            g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+
+            g2.setColor(getForeground());
+            g2.setFont(new Font("Arial", Font.PLAIN, 14));
+            FontMetrics metrics = g2.getFontMetrics();
+            int x = (getWidth() - metrics.stringWidth(getText())) / 2;
+            int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
+            g2.drawString(getText(), x, y);
+
+            g2.dispose();
+        }
+
+        @Override
+        protected void paintBorder(Graphics g) {
+        }
+    };
+
         closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         closeButton.setMaximumSize(new Dimension(500, 40));
         closeButton.setPreferredSize(new Dimension(500, 40));
         closeButton.setBackground(new Color(80, 64, 153));
         closeButton.setForeground(Color.WHITE);
+
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,6 +82,26 @@ public class HistoryPanel extends JPanel {
                 }
             }
         });
+
+        // Hover and click effect
+        closeButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                closeButton.setBackground(new Color(171, 160, 213)); // Change color on hover
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                closeButton.setBackground(new Color(80, 64, 153)); // Restore original color
+            }
+
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                closeButton.setBackground(new Color(41, 28, 98)); // Change color on click
+            }
+
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                closeButton.setBackground(new Color(171, 160, 213)); // Restore color after click
+            }
+        });
+
         contentPanel.add(Box.createRigidArea(new Dimension(0, 30)));
         contentPanel.add(closeButton);
 
