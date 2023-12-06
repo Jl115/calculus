c0dc8e10fa5e3c74cb5c4eb42439a405effee3ef
Stefan <stefan.mitric@ict.csbe.ch>
2023-12-06 18:13:27 +0100

    Ich habe die Methode zur berechnung von Pyramiden erstellt.

--- src/main/java/org/calculus/geometricSolids/PyramidCalculator.java
+++ src/main/java/org/calculus/geometricSolids/PyramidCalculator.java
@@ -1,4 +1,16 @@
 package org.calculus.geometricSolids;
 
 public class PyramidCalculator {
+    public static double calculateVolume(double sidelength, double height) {
+        return (sidelength * sidelength * height) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        double sidelength = 5.0; // Beispielwerte
+        double height = 10.0;
+
+        double volume = calculateVolume(sidelength, height);
+        System.out.println("Das Volumen der Pyramide ist: " + volume);
+    }
 }
+
