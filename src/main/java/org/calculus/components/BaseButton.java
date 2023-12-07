f26af66fdff2947225ef868f21df8fe28be2eae2
anukul <sommer.anukul99@gmail.com>
2023-12-07 22:10:21 +0100

    fixed the AC function. it now deletes both fields

--- src/main/java/org/calculus/components/BaseButton.java
+++ src/main/java/org/calculus/components/BaseButton.java
@@ -2,29 +2,79 @@ package org.calculus.components;
 
 import javax.swing.*;
 import java.awt.*;
+
+import org.calculus.UI.CallculatorPanel;
+import org.calculus.bracketsAndChainBills.MathExpressionEvaluator;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
+import java.math.BigDecimal;
 
 public class BaseButton extends JButton implements ActionListener {
-    private int fontSize;
+    private DisplayTextField displayTextField;
+    private CallculationTextField callculationTextField;
 
-    public BaseButton(String name) {
-        this(name, 20);
-    }
+    private String value = "0.0";
 
-    public BaseButton(String name, int fontSize) {
-        this.fontSize = fontSize;
+    public BaseButton(String name, DisplayTextField displayTextField , CallculationTextField callculationTextField) {
+        this.displayTextField = displayTextField;
+        this.callculationTextField = callculationTextField;
         this.setText(name);
         this.setContentAreaFilled(false);
         this.setFocusPainted(false);
         this.addActionListener(this);
-        this.setVisible(true);
+        setValue(null); // Dies wird in setSpecialValue
     }
 
-    public void setFontSize(int fontSize) {
-        this.fontSize = fontSize;
-        repaint();
+    public String setValue(String value) {
+        if ("1".equalsIgnoreCase(getText())) {
+            this.value = "1";
+        } else if ("2".equalsIgnoreCase(getText())) {
+            this.value = "2";
+        } else if ("3".equalsIgnoreCase(getText())) {
+            this.value = "3";
+        } else if ("4".equalsIgnoreCase(getText())) {
+            this.value = "4";
+        } else if ("5".equalsIgnoreCase(getText())) {
+            this.value = "5";
+        } else if ("6".equalsIgnoreCase(getText())) {
+            this.value = "6";
+        } else if ("7".equalsIgnoreCase(getText())) {
+            this.value = "7";
+        } else if ("8".equalsIgnoreCase(getText())) {
+            this.value = "8";
+        } else if ("9".equalsIgnoreCase(getText())) {
+            this.value = "9";
+        } else if ("0".equalsIgnoreCase(getText())) {
+            this.value = "0";
+        } else if ("/".equalsIgnoreCase(getText())) {
+            this.value = "/";
+        } else if ("AC".equalsIgnoreCase(getText())) {
+            this.value = "AC";
+        } else if ("+/-".equalsIgnoreCase(getText())) {
+            displayTextField.clear();
+        } else if ("mod".equalsIgnoreCase(getText())) {
+            this.value = "mod";
+        } else if ("*".equalsIgnoreCase(getText())) {
+            this.value = "*";
+        } else if ("-".equalsIgnoreCase(getText())) {
+            this.value = "-";
+        } else if ("+".equalsIgnoreCase(getText())) {
+            this.value = "+";
+        } else if ("=".equals(getText())) {
+            this.value = "=";
+        } else if (".".equalsIgnoreCase(getText())) {
+            this.value = ".";
+        } else if ("pi".equalsIgnoreCase(getText())) {
+            this.value = "3.14159265358979323846";
+        } else if ("e".equalsIgnoreCase(getText())) {
+            this.value = "2.7182818284590452354";
+        } else {
+            this.value = "0.0";
+        }
+        return value;
+
     }
 
     @Override
@@ -36,7 +86,7 @@ public class BaseButton extends JButton implements ActionListener {
         g2.fill(new RoundRectangle2D.Double(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));
 
         g2.setColor(getForeground());
-        g2.setFont(new Font("Arial", Font.PLAIN, fontSize));
+        g2.setFont(new Font("Arial", Font.PLAIN, 16));
         FontMetrics metrics = g2.getFontMetrics();
         int x = (getWidth() - metrics.stringWidth(getText())) / 2;
         int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
@@ -47,11 +97,24 @@ public class BaseButton extends JButton implements ActionListener {
 
     @Override
     protected void paintBorder(Graphics g) {
+        // Do not paint a border
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(this.getText());
+        if (this.value.equals("AC")) {
+            displayTextField.clear();
+            callculationTextField.clear();
+        } else if (this.value.equals("=")) {
+            Double result = MathExpressionEvaluator.calculate(displayTextField.getValue());
+            System.out.println(result);
+            if (!callculationTextField.getValue().isEmpty()) {
+                callculationTextField.clear();
+                
+            }
+            callculationTextField.setValue(result.toString());
+        } else {
+            displayTextField.setValue(this.value.toString());
+        }
     }
 }
-
--- src/main/java/org/calculus/components/CallculationTextField.java
+++ src/main/java/org/calculus/components/CallculationTextField.java
@@ -2,21 +2,18 @@ package org.calculus.components;
 
 import javax.swing.JTextField;
 
-public class DisplayTextField extends JTextField {
+public class CallculationTextField extends JTextField{
+     private String value = "calculation";
 
-    private String value = "0.0";
-
-    public DisplayTextField() {
+    public CallculationTextField() {
         super(10); 
-
+        this.setHorizontalAlignment(JTextField.RIGHT); // align text to the right
         this.setEditable(false);
-        this.setHorizontalAlignment(JTextField.RIGHT);
-
         setValue(value);
     }
 
     public void setValue(String value) {
-        if (this.value.equals("0.0")) {
+        if (this.value.equals("calculation")) {
         this.value = value;
         this.setText(value);
         this.repaint();
@@ -30,22 +27,17 @@ public class DisplayTextField extends JTextField {
         }
         
     }
-    public void setUpdatedValue(String value) {
-        this.value = String.valueOf(value);
-        this.setText(this.value);
-        this.repaint();
-        this.revalidate();
-    }
     public void clear() {
         System.out.println("Clear method called"); // Zum Debuggen
-        this.value = "0.0"; // Setzt den internen Wert
-        this.setText("0.0"); // Aktualisiert den Text im Textfeld
+        this.value = ""; // Setzt den internen Wert
+        this.setText(""); // Aktualisiert den Text im Textfeld
         this.repaint();
         this.revalidate();
     }
     
 
     public String getValue() {
+        System.out.println("getValue method called"); // Zum Debuggen
         return value;
     }
 }
