d2a5a9734cd972e49c9337016a0faba08dc56619
Miralem.Ba <miralembanovi@gmail.com>
2023-12-07 17:00:53 +0100

    adding new rules and fixing the bugs

--- src/main/java/org/calculus/geometricSolids/ConeCalculator.java
+++ src/main/java/org/calculus/geometricSolids/ConeCalculator.java
@@ -1,5 +1,15 @@
 package org.calculus.geometricSolids;
 
 public class ConeCalculator {
-    /*MB*/
+    /**
+     * Calculates the volume of a cone.
+     *
+     * @param r Radius of the base of the cone
+     * @param h Height of the cone
+     * @return Volume of the cone
+     */
+    public static double Volume(double r, double h) {
+        return (22 * r * r * h) / (3 * 7);
+    }
 }
+
