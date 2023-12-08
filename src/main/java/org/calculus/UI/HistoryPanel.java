012dcf8e196182d8af9e08cc129690aaeb415bb6
anukul <sommer.anukul99@gmail.com>
2023-12-08 01:19:52 +0100

    added custom fonts

--- src/main/java/org/calculus/UI/HistoryPanel.java
+++ src/main/java/org/calculus/UI/HistoryPanel.java
@@ -1,22 +1,20 @@
 package org.calculus.UI;
 
-import org.calculus.components.DisplayTextField;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class HistoryPanel extends JPanel {
 
-    private DisplayTextField displayTextField;
-
-    public HistoryPanel(DisplayTextField displayTextField) {
-        this.displayTextField = displayTextField;
+    public HistoryPanel() {
         setLayout(new BorderLayout());
         setBackground(new Color(19, 22, 27));
+
+        // Custom font file path
+        String customFontPath = ".\\SometypeMono-VariableFont_wght.ttf";
 
         // Create a panel to hold the text fields and the button
         JPanel contentPanel = new JPanel();
@@ -24,10 +22,10 @@ public class HistoryPanel extends JPanel {
         contentPanel.setBackground(new Color(19, 22, 27));
         contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        // Color for the textfields
+        // Color for the text fields
         Color textFieldColor = new Color(162, 148, 225, 96);
 
-        // Create the textfields
+        // Create the text fields
         for (int i = 0; i < 10; i++) {
             JTextField textField = new JTextField() {
                 @Override
@@ -38,10 +36,19 @@ public class HistoryPanel extends JPanel {
                     g2.fillRect(0, 0, getWidth(), getHeight());
                     g2.dispose();
 
-                    super.paintComponent(g);
+                    try {
+                        Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File(customFontPath)).deriveFont(Font.BOLD, 14);
+
+                        g2.setColor(getForeground());
+                        g2.setFont(customFont);
+                        super.paintComponent(g);
+                    } catch (IOException | FontFormatException e) {
+                        e.printStackTrace();
+                        g2.setColor(getForeground());
+                        super.paintComponent(g);
+                    }
                 }
             };
-            textField.setText(displayTextField.getText());
             textField.setPreferredSize(new Dimension(200, 30));
             textField.setEditable(false);
             textField.setOpaque(false);
@@ -52,35 +59,43 @@ public class HistoryPanel extends JPanel {
 
         // Create the "Close History" button
         JButton closeButton = new JButton("Close History") {
-        @Override
-        protected void paintComponent(Graphics g) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(getBackground());
+                g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
 
-            g2.setColor(getBackground());
-            g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+                // Change font settings here
+                try {
+                    Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File(customFontPath)).deriveFont(Font.BOLD, 14);
 
-            g2.setColor(getForeground());
-            g2.setFont(new Font("Arial", Font.PLAIN, 14));
-            FontMetrics metrics = g2.getFontMetrics();
-            int x = (getWidth() - metrics.stringWidth(getText())) / 2;
-            int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
-            g2.drawString(getText(), x, y);
+                    g2.setColor(getForeground());
+                    g2.setFont(customFont);
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int x = (getWidth() - metrics.stringWidth(getText())) / 2;
+                    int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
+                    g2.drawString(getText(), x, y);
+                } catch (IOException | FontFormatException e) {
+                    e.printStackTrace();
+                    // Handle font loading error
+                    g2.setColor(getForeground());
+                    super.paintComponent(g);
+                }
 
-            g2.dispose();
-        }
+                g2.dispose();
+            }
 
-        @Override
-        protected void paintBorder(Graphics g) {
-        }
-    };
-
+            @Override
+            protected void paintBorder(Graphics g) {
+                // Do not paint a border
+            }
+        };
         closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         closeButton.setMaximumSize(new Dimension(500, 40));
         closeButton.setPreferredSize(new Dimension(500, 40));
         closeButton.setBackground(new Color(80, 64, 153));
         closeButton.setForeground(Color.WHITE);
-
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -90,56 +105,10 @@ public class HistoryPanel extends JPanel {
                 }
             }
         });
-
-        // Hover and click effect
-        closeButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                closeButton.setBackground(new Color(171, 160, 213)); // Change color on hover
-            }
-
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                closeButton.setBackground(new Color(80, 64, 153)); // Restore original color
-            }
-
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                closeButton.setBackground(new Color(41, 28, 98)); // Change color on click
-            }
-
-            public void mouseReleased(java.awt.event.MouseEvent evt) {
-                closeButton.setBackground(new Color(171, 160, 213)); // Restore color after click
-            }
-        });
-
         contentPanel.add(Box.createRigidArea(new Dimension(0, 30)));
         contentPanel.add(closeButton);
 
         // Add the content panel to the HistoryPanel
         add(contentPanel, BorderLayout.CENTER);
-    }
-
-    public void shiftAndSet(String newText) {
-        // Retrieve all text fields previously created
-        Component[] components = getComponents();
-        JTextField[] textFields = new JTextField[components.length];
-
-        int index = 0;
-        for (Component comp : components) {
-            if (comp instanceof JTextField) {
-                textFields[index] = (JTextField) comp;
-                index++;
-            }
-        }
-
-        // Shift content in text fields downward
-        for (int i = textFields.length - 1; i > 0; i--) {
-            if (textFields[i - 1] != null) {
-                textFields[i].setText(textFields[i - 1].getText());
-            }
-        }
-
-        // Set the new text in the first field
-        if (textFields.length > 0) {
-            textFields[0].setText(newText);
-        }
     }
 }
